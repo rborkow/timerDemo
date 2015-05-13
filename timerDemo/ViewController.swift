db558a80caa552bb097fcc007573495b15a1def2
rborkows <rborkows@gmail.com>
2015-05-13 15:06:02 -0700

    Working version

--- timerDemo/ViewController.swift
+++ timerDemo/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var secondsRemaining: Int = 60
+    var votes: Int = 1000000
+    var voteFormat = NSNumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        voteFormat.numberStyle = .DecimalStyle
+        voteFormat.groupingSeparator = ","
+        voteFormat.groupingSize = 3
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +33,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onStartButtonPressed(sender: AnyObject) {
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1,
+            target: self,
+            selector: Selector("updateLabels:"),
+            userInfo: nil,
+            repeats: true
+        )
+        startButton.enabled = false
+    }
+    
+    func updateLabels(timer: NSTimer) {
+        println(secondsRemaining)
+        if secondsRemaining == 0 {
+            timer.invalidate()
+            startButton.enabled = true
+        } else {
+            secondsRemaining--
+            timerLabel.text = "00:00:\(secondsRemaining)"
+            
+            votes += Int(arc4random_uniform(1000)+1)
+            counterLabel.text = voteFormat.stringFromNumber(votes)
+        }
+    }
 
 }
 
